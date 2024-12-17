639ca4a0528862a119bfd96b9e6885990ab2f058
BugHunter-X <askzenai@gmail.com>
2024-12-17 20:50:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+```javascript
+//Incorrect usage of useState hook
+import React, { useState } from 'react';
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1); //This is incorrect
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>
+  );
+}
+
+export default MyComponent;
+```
